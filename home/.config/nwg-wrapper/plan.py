4365364317e5f9fed269891c8a6ebab4cf47b1d9
Cristóbal Tapia Camú <crtapia@gmail.com>
2021-08-13 01:28:41 +0200

    Update nwr-wrapper script

--- home/.config/nwg-wrapper/plan.py
+++ home/.config/nwg-wrapper/plan.py
@@ -26,51 +26,56 @@ def format_output(data):
     TODO
 
     """
-    width = 35
+    width = 34
     year = date.today().year
     next_year = year + 1
-    list_plan = "".ljust(12) + f"{year}".ljust(width) + f"{next_year}".ljust(width) + "\n"
+    list_plan1 = "".ljust(5) + f"{year}".ljust(width) + "\n"
+    list_plan2 = "".ljust(5) + f"{next_year}".ljust(width) + "\n"
+
     for k in data.index:
         elements_1 = str(data.loc[k, year]).split("/")
         elements_2 = str(data.loc[k, next_year]).split("/")
-
-        elements_month = max(len(elements_1), len(elements_2))
 
         month = shorten(f"{k}", 3, placeholder="") + "  "
 
         list_1 = []
         list_2 = []
 
-        for j in range(elements_month):
-            m1 = elements_1[j] if j < len(elements_1) else ""
-            m2 = elements_2[j] if j < len(elements_2) else ""
-
-            if m1 == "nan":
+        for j, e in enumerate(elements_1):
+            if e == "nan":
                 m1 = "".ljust(width)
             else:
-                m1 = shorten(str(m1), width).ljust(width)
+                m1 = shorten(str(e), width).ljust(width)
 
             list_1.append(m1)
 
-            if m2 == "nan":
+        for j, e in enumerate(elements_2):
+            if e == "nan":
                 m2 = "".ljust(width)
             else:
-                m2 = shorten(str(m2), width).ljust(width)
+                m2 = shorten(str(e), width).ljust(width)
 
             list_2.append(m2)
 
         start = "".ljust(5)
-        for k in range(len(list_1)):
-            month1 = f'<span foreground="#81a1c1">{list_1[k]}</span>'
-            month2 = f'<span foreground="#ebcb8b">{list_2[k]}</span>'
-            if k > 0:
-                list_plan += f"{start}{month1} {start}{month2}\n"
+        for l, e in enumerate(list_1):
+            month1 = f'<span foreground="#81a1c1">{e}</span>'
+            if l > 0:
+                list_plan1 += f"{start}{month1}\n"
             else:
-                list_plan += f"{month}{month1} {month}{month2}\n"
+                list_plan1 += f"{month}{month1}\n"
+
+        for l, e in enumerate(list_2):
+            month2 = f'<span foreground="#ebcb8b">{e}</span>'
+            if l > 0:
+                list_plan2 += f"{start}{month2}\n"
+            else:
+                list_plan2 += f"{month}{month2}\n"
 
     base = f"""<span face="monospace">
 <span foreground="#eeeeee">
-{list_plan}
+{list_plan1}
+{list_plan2[:-1]}
 </span>
 </span>"""
 
