2aeec6f4d75164c1907455a8535091ed94da26c9
Cristóbal Tapia Camú <crtapia@gmail.com>
2021-10-10 13:34:11 +0200

    Update fzf colors

--- home/.fzf.bash
+++ home/.fzf.bash
@@ -71,3 +71,6 @@ _gs() {
   git stash list | fzf-down --reverse -d: --preview 'git show --color=always {1}' |
   cut -d: -f1
 }
+
+# Color theme
+export FZF_DEFAULT_OPTS='--color=bg+:#3B4252,bg:#2E3440,spinner:#81A1C1,hl:#616E88,fg:#D8DEE9,header:#616E88,info:#81A1C1,pointer:#81A1C1,marker:#81A1C1,fg+:#D8DEE9,prompt:#81A1C1,hl+:#81A1C1'
