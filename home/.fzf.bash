b1ccda097d38168045625ec8b2159a9b8636e5db
Cristóbal Tapia Camú <crtapia@gmail.com>
2021-10-11 00:15:33 +0200

    update fzf config

--- home/.fzf.bash
+++ home/.fzf.bash
@@ -74,3 +74,12 @@ _gs() {
 
 # Color theme
 export FZF_DEFAULT_OPTS='--color=bg+:#3B4252,bg:#2E3440,spinner:#81A1C1,hl:#616E88,fg:#D8DEE9,header:#616E88,info:#81A1C1,pointer:#81A1C1,marker:#81A1C1,fg+:#D8DEE9,prompt:#81A1C1,hl+:#81A1C1'
+
+#determines search program for fzf
+if type ag &> /dev/null; then
+    export FZF_DEFAULT_COMMAND='ag -p ~/.gitignore -g ""'
+fi
+#refer rg over ag
+if type rg &> /dev/null; then
+    export FZF_DEFAULT_COMMAND='rg --files --hidden'
+fi
