a4d9feb450dcc14e84c1236e61d9e2a6d033f5c1
Cristóbal Tapia Camú <crtapia@gmail.com>
2023-05-22 14:59:38 +0200

    nvim: fix keymapings for leap.nvim

--- home/.config/nvim/after/plugin/leap.lua
+++ home/.config/nvim/after/plugin/leap.lua
@@ -1,3 +1,3 @@
 vim.api.nvim_set_hl(0, 'LeapBackdrop', { link = 'Comment' })
-vim.keymap.set('n', 's', '<Plug>(leap-forward-to)')
-vim.keymap.set('n', 'S', '<Plug>(leap-backward-to)')
+vim.keymap.set('n', 's', '<Plug>(leap-forward-to)', { silent = true })
+vim.keymap.set('n', 'S', '<Plug>(leap-backward-to)', { silent = true })
