74032eee5104b88c2f01e63892041912781b40ce
Cristóbal Tapia Camú <crtapia@gmail.com>
2023-06-17 22:25:56 +0200

    nvim: update overseer template for python

--- home/.config/nvim/lua/overseer/template/user/python_build.lua
+++ home/.config/nvim/lua/overseer/template/user/python_build.lua
@@ -6,12 +6,19 @@ return {
         return {
             cmd = { "python" },
             args = { file },
-            components = { {
-                "on_output_quickfix",
-                open = true,
-                items_only = false,
-                tail = true,
-            }, "default" },
+            components = {
+                { "on_exit_set_status" },
+                {
+                    "display_duration",
+                    detail_level = 2,
+                },
+                {
+                    "on_output_quickfix",
+                    open = true,
+                    items_only = false,
+                    tail = true,
+                },
+            },
         }
     end,
     condition = {
