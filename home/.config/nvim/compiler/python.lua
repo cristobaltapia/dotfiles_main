d3eb08fd5905772d8fadbe24c6e44f24df26506b
Cristóbal Tapia Camú <crtapia@gmail.com>
2023-08-02 14:46:10 +0200

    nvim: errorformat for python

--- home/.config/nvim/compiler/python.lua
+++ home/.config/nvim/compiler/python.lua
@@ -3,16 +3,17 @@ vim.opt.cpoptions:remove("C")
 
 -- Set error format
 vim.cmd([[set errorformat=
-\%A\ \ File\ \"%f\"\\\,\ line\ %l\\\,\ in\ %o%\\C,
-\%A\ \ File\ \"%f\"\\\,\ line\ %l%\\C,
-\%f:%l:\ Runtime%tarning%m,
-\%-G\ \ %.%#,
-\%W%f:%l:\ %m,
+\%E\ \ File\ \"%f\"\\\,\ line\ %l\\\,\ in\ %o%\\C,
+\%E\ \ File\ \"%f\"\\\,\ line\ %l%\\C,
+\%W%f:%l:\ %.%#%tarning%m,
+\%+Z%.%#%trror:\ %.%#,
 \%C%p^,
 \%-C\ \ \ \ %.%#,
 \%-C\ \ %.%#,
+\%-Z,
+\%-Z\ \ %.%#,
 \%+C%.%#,
-\%Z%\\S%\\&%m,
+\%-G\ \ %.%#,
 \%-G%.%#
 ]]
 )
