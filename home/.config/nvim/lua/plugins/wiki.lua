29447e2f0c6533b777d344a9a8cdce46044972ca
Cristóbal Tapia Camú <crtapia@gmail.com>
2023-10-04 18:18:07 +0200

    neovim: improve wiki.vim config

--- home/.config/nvim/lua/plugins/wiki.lua
+++ home/.config/nvim/lua/plugins/wiki.lua
@@ -6,28 +6,34 @@ return {
     dependencies = {
       { 'lervag/wiki-ft.vim' },
     },
-    tag = "v0.8",
+    -- tag = "v0.8",
     cmd = "WikiIndex",
     init = function()
+      -- Root folder
+      vim.g.wiki_root = '~/Nextcloud/Notes'
+      -- Keymaps
+      vim.api.nvim_set_keymap("n", "<leader>wst", ":WikiTags<cr>" , {desc = "Search tags."})
+      -- Define location of pubs libraries
       vim.g.vimwiki_pubs_config = {
         vim.env.HOME .. "/.config/pubs/main_library.conf",
         vim.env.HOME .. "/.config/pubs/misc_library.conf",
       }
-
-      vim.g.wiki_root = '~/Nextcloud/Notes'
+      -- Cretion of links
       vim.g.wiki_link_creation = {
         wiki = {
           link_type = "wiki",
           url_extension = ".wiki",
           url_transform = function(x)
             local name
-            name, _ = string.gsub(string.lower(x), " ", "-")
+            name, _ = string.gsub(string.lower(x), "[ :]",
+              { [" "] = "-", [":"] = "" }
+            )
             return name
           end
         }
       }
+      -- Filetypes
       vim.g.wiki_filetypes = { 'wiki' }
-      vim.g.wiki_select_method = 'fzf'
     end
   },
   {
