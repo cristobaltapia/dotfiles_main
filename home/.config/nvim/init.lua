05c604b2c981d97e6b2d3610b2b86195053d4fcc
Cristóbal Tapia Camú <crtapia@gmail.com>
2023-10-20 13:44:22 +0200

    nvim: set localleader

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -14,6 +14,7 @@ vim.opt.rtp:prepend(lazypath)
 
 -- Define the leader key
 vim.g.mapleader = ","
+vim.g.maplocalleader = ","
 
 -- Load further configurations (mappings, sets, etc)
 require("tapia")
