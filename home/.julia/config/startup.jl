b05de400a485afcbc8677791ecbe28605570c034
Cristóbal Tapia Camú <crtapia@gmail.com>
2024-05-24 01:00:33 -0700

    update julia startup script

--- home/.julia/config/startup.jl
+++ home/.julia/config/startup.jl
@@ -3,3 +3,9 @@ try
 catch e
     @warn "Error initializing Revise" exception=(e, catch_backtrace())
 end
+
+try
+    using Debugger
+catch e
+    @warn "Error initializing Debugger" exception=(e, catch_backtrace())
+end
