95ec192c8fbd23ac8b54507479e504853407cfb3
Cristóbal Tapia Camú <crtapia@gmail.com>
2024-12-04 21:07:50 +0100

    nvim: arduino support treesitter

--- home/.config/nvim/lua/plugins/treesitter.lua
+++ home/.config/nvim/lua/plugins/treesitter.lua
@@ -8,6 +8,7 @@ return {
     opts = {
       -- A list of parser names, or "all" (the five listed parsers should always be installed)
       ensure_installed = {
+        "arduino",
         "bash",
         "bibtex",
         "comment",
