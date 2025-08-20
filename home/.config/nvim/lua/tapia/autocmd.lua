2c1c31c457027702b7db21e72626f93811147aa2
Cristóbal Tapia Camú <crtapia@gmail.com>
2025-08-20 21:10:51 +0200

    nvim: some changes

--- home/.config/nvim/lua/tapia/autocmd.lua
+++ home/.config/nvim/lua/tapia/autocmd.lua
@@ -26,6 +26,20 @@ vim.api.nvim_create_autocmd({ "User" }, {
 vim.api.nvim_create_autocmd({ "InsertLeave" }, {
   pattern = { "*" },
   desc = "Reload folding every time insert mode is left.",
-  command = "set foldmethod=expr"
-
+  command = "set foldmethod=expr",
 })
+
+-- vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI", "BufEnter" }, {
+--   group = vim.api.nvim_create_augroup("ScrollOffEOF", {}),
+--   callback = function()
+--     local win_h = vim.api.nvim_win_get_height(0)
+--     local off = vim.o.scrolloff
+--     local dist = vim.fn.line("$") - vim.fn.line(".")
+--     local rem = vim.fn.line("w$") - vim.fn.line("w0") + 1
+--     if dist < off and win_h - rem + dist < off then
+--       local view = vim.fn.winsaveview()
+--       view.topline = view.topline + off - (win_h - rem + dist)
+--       vim.fn.winrestview(view)
+--     end
+--   end,
+-- })
