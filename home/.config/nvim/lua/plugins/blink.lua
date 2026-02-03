5d5852264486171328e54fe98a6fd68cb6eaf141
Cristóbal Tapia Camú <crtapia@gmail.com>
2026-02-03 19:34:34 +0100

    nvim: update confi

--- home/.config/nvim/lua/plugins/blink.lua
+++ home/.config/nvim/lua/plugins/blink.lua
@@ -2,7 +2,7 @@ return {
   {
     "saghen/blink.compat",
     -- use the latest release, via version = '*', if you also use the latest release for blink.cmp
-    version = "v1.7.0",
+    version = "v1.9.0",
     -- lazy.nvim will automatically load the plugin when it's required by blink.cmp
     lazy = true,
     -- make sure to set opts so that lazy.nvim calls blink.compat's setup
